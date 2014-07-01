603e6ff2d8127f0c6e20742019358cd8ac537236
Driesssens <hsjdriessens@gmail.com>
2014-07-01 23:27:00 +0200

    Javadoc-feestje

--- bramspr/Bramspr.java
+++ bramspr/Bramspr.java
@@ -12,10 +12,21 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 
+/**
+ * The driver class for the Bramspr compiler. Contains the main method.
+ */
 public class Bramspr {
+
+	/**
+	 * The method to use to compile a Bramspr program. Provide a filename as argument. The filename should refer to a Bramspr file. This method then lexes,
+	 * parses, context-checks and finally compiles the program to a Java Bytecode .class-file. During this process, the program will be checked for syntactic or
+	 * contextual errors, and will report those when encountered.
+	 * 
+	 * @param args The filename of the Bramspr-file that is to be compiled.
+	 */
 	public static void main(String[] args) throws Exception {
 		System.out.println("**** Bramspr Compiler v0.1.0 ****");
-		
+
 		String inputFile = null;
 		if (args.length > 0)
 			inputFile = args[0];
@@ -24,29 +35,31 @@ public class Bramspr {
 			is = new FileInputStream(inputFile);
 		}
 		CharStream input = new UnbufferedCharStream(is);
-		
-		System.out.println("Start lexing");
+
+		System.out.println("Start lexing...");
 		BramsprLexer lex = new BramsprLexer(input);
 		lex.setTokenFactory(new CommonTokenFactory(true));
 		TokenStream tokens = new UnbufferedTokenStream<CommonToken>(lex);
-		
-		System.out.println("Start parsing");
-		BramsprParser parser = new BramsprParser(tokens);		
-		ParseTree tree = parser.program(); // parse!
-		
-		System.out.println("Start checking");
+
+		System.out.println("Start parsing...");
+		BramsprParser parser = new BramsprParser(tokens);
+		ParseTree tree = parser.program();
+
+		System.out.println("Start checking...");
 		BramsprChecker checker = new BramsprChecker();
 		checker.check(tree);
-		System.out.println("Finished checking");
-		
-		System.out.println("Start compiling");
+		System.out.println("Finished checking.");
+
+		System.out.println("Start compiling...");
 		BramsprCompiler compiler = new BramsprCompiler();
 		byte[] code = compiler.compile(tree);
-		System.out.println("Finished compiling");
+		System.out.println("Finished compiling.");
+
+		String targetFileName = inputFile.subSequence(0, inputFile.indexOf(".spr")).toString() + ".class";
+		System.out.println("Writing the generated code to " + targetFileName);
 		
-		System.out.println("Writing code to file");
-		FileOutputStream fos = new FileOutputStream("Example.class");
-        fos.write(code);
-        fos.close();
+		FileOutputStream fos = new FileOutputStream(targetFileName);
+		fos.write(code);
+		fos.close();
 	}
 }
